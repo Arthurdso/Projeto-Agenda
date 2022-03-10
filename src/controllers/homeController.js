7859918c1990eb10e3b846fc54473c1fa74d4d52
Arthurdso <arthurdso96@gmail.com>
2022-03-10 19:10:01 -0300

    Criando Pagina de Login

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,12 +1,5 @@
-exports.paginaInicial = (req, res) => {
-    res.render('index',{
-        titulo: 'Este será o titulo da pagina',
-        numeros: [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    });
+exports.index = (req, res) => {
+    res.render('index')
     return;
 };
 
-exports.trataPost = (req, res) => {
-    res.send(req.body);
-    return;
-};
