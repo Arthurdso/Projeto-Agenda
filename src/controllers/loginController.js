d4da0aa9b10fb8044af6d15287bb0c6e20951876
Arthurdso <arthurdso96@gmail.com>
2022-03-11 15:15:03 -0300

    add node modules for test

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -26,3 +26,27 @@ exports.register = async function(req, res) {
         return res.render('404');
     }
 };
+
+exports.login = async function(req, res) {
+    try {
+        const login = new Login(req.body);
+        await login.login();
+    
+        if(login.errors.length > 0) {
+            req.flash('errors', login.errors);
+            req.session.save(function() {
+            return res.redirect('/login/index');
+            });
+            return;
+        }
+
+        req.flash('success', 'Você entrou no sistema.');
+        req.session.user = login.user;
+        req.session.save(function() {
+        return res.redirect('/login/index');
+        });
+    } catch(e) {
+        console.log(e);
+        return res.render('404');
+    }
+};
--- src/models/LoginModel.js
+++ src/models/LoginModel.js
@@ -16,6 +16,22 @@ class Login {
         this.user = null;
     }
 
+    async login() {
+        this.valida();
+        if (this.errors.length > 0) return;
+        this.user = await LoginModel.findOne({ email: this.body.email });
+
+        if(!this.user) {
+            this.errors.push('Usuário não existe.');
+            return;
+    }
+        if(!bcryptjs.compareSync(this.body.password, this.user.password)) {
+            this.errors.push('Senha inválida');
+            this.user = null;
+            return;
+        }
+    }
+
     async register() {
         this.valida();
         if (this.errors.length > 0) return;
@@ -27,17 +43,14 @@ class Login {
         const salt = bcrypt.genSaltSync();
         this.body.password = bcryptjs.hashSync(this.body.password, salt);
 
-        try {
-            this.user = await LoginModel.create(this.body);
-        } catch (e) {
-            console.log(e);
-        }
+
+        this.user = await LoginModel.create(this.body);
     }
 
     async userExists() {
-        const user = await LoginModel.findOne({ email: this.body.email });
-
-        if(user) this.errors.push('Usuário já existe.');
+        this.user = await LoginModel.findOne({ email: this.body.email });
+        if (this.user) this.errors.push('Usuário já existe.');
+        
     }
 
     valida() {
