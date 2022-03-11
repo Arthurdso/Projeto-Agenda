3f30bebdf3ce979a9b30a976a9d53d535cf81931
Arthurdso <arthurdso96@gmail.com>
2022-03-11 16:10:00 -0300

    Create login sistem

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -1,7 +1,8 @@
 const Login = require('../models/LoginModel')
 
 exports.index =(req, res) => {
-    res.render('login');
+    if(req.session.user) return res.render('login-logado')
+    return res.render('login');
 };
 
 exports.register = async function(req, res) {
@@ -49,4 +50,9 @@ exports.login = async function(req, res) {
         console.log(e);
         return res.render('404');
     }
+};
+
+exports.logout = function(req, res) {
+    req.session.destroy();
+    res.redirect('/');
 };
