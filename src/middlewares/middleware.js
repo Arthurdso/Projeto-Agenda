e193dddc059f5899d7c05e8fab4bfbb41d42e731
Arthurdso <arthurdso96@gmail.com>
2022-03-10 21:03:04 -0300

    Criação pagina de login
    Criação Pagina de Login e validação de Email ja existente na base de dados.

--- src/middlewares/middleware.js
+++ src/middlewares/middleware.js
@@ -1,5 +1,6 @@
 exports.middlewareGlobal = (req, res, next) => {
-  res.locals.umaVariavelLocal = 'Este é o valor da variavel local.'
+  res.locals.errors = req.flash('errors');
+  res.locals.success = req.flash('success');
   next();
 };
 
